c9f772c4dcd958ebc9ae01d1afea71c809082c96
David Martinez <davidmgilo@gmail.com>
2016-10-25 17:03:15 +0200

    to 15 iguals

--- spec/TennisGameSpec.php
+++ spec/TennisGameSpec.php
@@ -2,12 +2,24 @@
 
 namespace spec;
 
+use Player;
 use TennisGame;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class TennisGameSpec extends ObjectBehavior
 {
+
+    protected $player1;
+    protected $player2;
+
+    function let(){
+
+        $this->player1 = new Player("Maria",0);
+        $this->player2 = new Player("Pepe",0);
+        $this->beConstructedWith($this->player1, $this->player2);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType(TennisGame::class);
@@ -18,10 +30,13 @@ class TennisGameSpec extends ObjectBehavior
     }
 
     function it_score_15_0 (){
+        $this->player1->winpoint();
         $this->score()->shouldReturn('15 - 0');
     }
 
     function it_score_15_iguals (){
+        $this->player1->winpoint();
+        $this->player2->winpoint();
         $this->score()->shouldReturn('15 iguals');
     }
 }
